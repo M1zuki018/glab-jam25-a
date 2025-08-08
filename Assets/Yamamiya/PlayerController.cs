d0b9cca393720eb7eb112739661599ef6cf5c01b
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2025-08-08 15:58:41 +0900

    [update] プレイヤーが近くの遮蔽物を取得して隠れるようにした。プレイヤーが遮蔽物に隠れる前のx座標に戻れるようにした。

--- Assets/Yamamiya/PlayerController.cs
+++ Assets/Yamamiya/PlayerController.cs
@@ -1,31 +1,41 @@
 ﻿using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Transform targetPos;
-    [SerializeField] Transform startPos; 
-    [SerializeField,Tooltip("ターゲットとの距離")] float distance;
+    [SerializeField] Transform _targetPos;
+    [SerializeField] Vector3 _startPos; 
+    [SerializeField,Tooltip("ターゲットとの距離")] float _distance;
 
     [Header("Player")]
-    [SerializeField] Transform player;
-    [SerializeField,Tooltip("速度")] float speed;
-    [SerializeField,Tooltip("移動させるキー")] KeyCode moveKey;
+    Transform _player;
+    [SerializeField,Tooltip("速度")] float _speed;
+    [SerializeField,Tooltip("移動させるキー")] KeyCode _moveKey;
+    GameObject _nearShelter;
+    [SerializeField,Tooltip("継続時間")] float _duration;
 
     private void Start()
     {
-        player = GetComponent<Transform>();
-        startPos = player;
-        distance = targetPos.position.z - player.position.z;
+        _player = GetComponent<Transform>();
+        _startPos = _player.transform.position;
+        _distance = _targetPos.position.z - _player.position.z;
     }
 
     void Update()
     {
-        if (Input.GetKey(moveKey))
+        if (Input.GetKey(_moveKey))
         {
             MovePlayer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HiddenShelter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            BackOriginalPosX();
         }
     }
 
@@ -34,13 +44,7 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     public void MovePlayer()
     {
-        player.position += Vector3.forward * speed * Time.deltaTime;
-        UpdateDistance();
-    }
-
-    private void UpdateDistance()
-    {
-        Debug.Log(GetDistance());
+        _player.position += Vector3.forward * _speed * Time.deltaTime;
     }
 
     /// <summary>
@@ -49,7 +53,50 @@ public class PlayerController : MonoBehaviour
     /// <returns></returns>
     public float GetDistance()
     {
-        return player.position.z / distance;
+        return _player.position.z / _distance;
+    }
+
+    /// <summary>
+    /// 遮蔽物に隠れる
+    /// </summary>
+    public void HiddenShelter()
+    {
+        _nearShelter = FindSearchNearShelter();
+        if(_nearShelter != null)
+        {
+            _player.DOMove(_nearShelter.transform.position - Vector3.forward, _duration);
+        }
+    }
+
+    /// <summary>
+    /// 一番近い遮蔽物を取得する
+    /// </summary>
+    /// <returns></returns>
+    private GameObject FindSearchNearShelter()
+    {
+        GameObject[] shelters;
+        shelters = GameObject.FindGameObjectsWithTag("Shelter"); 
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = _player.position;
+        Vector3 foward = _player.forward;
+        foreach (GameObject shelter in shelters)
+        {
+            Vector3 directionTarget = shelter.transform.position - position;
+            float curDistance = directionTarget.sqrMagnitude;
+            float dotProduct = Vector3.Dot(foward, directionTarget);
+            if (curDistance < distance && dotProduct > 0)
+            {
+                closest = shelter;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    public void BackOriginalPosX()
+    {
+        _player.DOMoveX(_startPos.x, _duration);
     }
 
     private void OnTriggerEnter(Collider other)
