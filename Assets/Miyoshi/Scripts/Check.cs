34e7032af13a7574668eafbf9be5614f5c43f70a
tomoyamiyoshikun <tomoyamiyoshikun@gmail.com>
2025-08-08 12:09:53 +0900

    [change]

---
+++ Assets/Miyoshi/Scripts/Check.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check : EnemyRotation
+{
+    private float timer;
+
+    public override void Update()
+    {
+        // まず親クラスのUpdateを実行して、intervalを更新
+        base.Update();
+
+        // その後、子クラス側の処理
+        timer += Time.deltaTime;
+        if (timer >=2)
+        {
+            Debug.Log("Check: " + interval);
+            timer = 0f;
+        }
+    }
+}
--- Assets/Miyoshi/Scripts/EnemyRotation.cs
+++ Assets/Miyoshi/Scripts/EnemyRotation.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class EnemyRotation : MonoBehaviour
 {
-    [SerializeField] private int max_Interval = 5; // 回転間隔(秒)
-    [SerializeField] private int min_Interval = 1; // 回転間隔(秒)
+    [SerializeField] private float max_Interval = 5; // 回転間隔(秒)
+    [SerializeField] private float min_Interval = 1; // 回転間隔(秒)
     [SerializeField] private float backInterval = 0.5f; // 後ろを向く間隔(秒)
     [SerializeField] private float rotateAngleY = 45f; // 1回あたりの回転角度(Y)
     bool isBack = false; // 後ろを向いているかどうか
 
-    public int interval = 3; // 回転間隔
+    public float interval = 3; // 回転間隔
     private float timer;
 
-    void Update()
+    public virtual void Update()
     {
         // 経過時間を加算
         timer += Time.deltaTime;
          // 回転間隔をランダムに設定
         // 一定時間経過したら回転
         if (timer >= interval)
-        {
-            
+        { 
             // 現在のRotationを取得してY軸に加算
             transform.rotation = Quaternion.Euler(
                 transform.rotation.eulerAngles.x,
@@ -50,6 +49,5 @@ public class EnemyRotation : MonoBehaviour
                 isBack = false; // 後ろを向く状態を解除
             }
         }
-
     }
 }
