f0ae62cedce202865e34acf83c83f28f07ec2fd6
hyotetsu99nmb <hyotetsu99nmb@gmail.com>
2025-08-08 15:08:47 +0900

    Reapply "[add]QTEControllerとQTETriggerを作成"
    This reverts commit ef77bc0a03622f4773e98d9833d404c49e2ad7e2.

---
+++ Assets/Hyougo/Script/QTETrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class QTETrigger : MonoBehaviour
+{
+    [SerializeField] private QTEController qteController;
+    [SerializeField] private float qteTimeLimit = 3f;
+
+    private bool hasActivated = false; // 一度だけ実行する用
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasActivated) return; // 連続発動防止
+
+        if (other.CompareTag("Player"))
+        {
+            hasActivated = true;
+            qteController.StartQTE(qteTimeLimit);
+        }
+    }
+}
