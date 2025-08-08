563d325b058657f52e6caaab0f396264be39f9b8
tomoyamiyoshikun <tomoyamiyoshikun@gmail.com>
2025-08-08 11:33:30 +0900

    [add]

---
+++ Assets/Miyoshi/Scripts/EnemyManeg.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManeg : MonoBehaviour
+{
+    // 自分自身
+    [SerializeField] private Transform _self;
+
+    // ターゲット
+    [SerializeField] private Transform _target;
+
+    // 障害物として判定するレイヤー
+    [SerializeField] private LayerMask _obstacleLayer;
+
+    // 視野角（度数法）
+    [SerializeField] private float _sightAngle;
+
+    // 視界の最大距離
+    [SerializeField] private float _maxDistance = float.PositiveInfinity;
+
+    #region Logic
+
+    /// <summary>
+    /// ターゲットが見えているかどうか
+    /// </summary>
+    public bool IsVisible()
+    {
+        // 自身の位置
+        var selfPos = _self.position;
+        // ターゲットの位置
+        var targetPos = _target.position;
+
+        // 自身の向き
+        var selfDir = _self.forward;
+
+        // ターゲットまでの向きと距離
+        var targetDir = targetPos - selfPos;
+        var targetDistance = targetDir.magnitude;
+
+        // 視野角判定
+        var cosHalf = Mathf.Cos(_sightAngle / 2 * Mathf.Deg2Rad);
+        var innerProduct = Vector3.Dot(selfDir, targetDir.normalized);
+        if (!(innerProduct > cosHalf && targetDistance < _maxDistance))
+        {
+            return false;
+        }
+
+        // 障害物判定（Raycast）
+        if (Physics.Raycast(selfPos, targetDir.normalized, out RaycastHit hit, _maxDistance, _obstacleLayer))
+        {
+            // Rayがターゲット以外に当たったら見えない
+            if (hit.transform != _target)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    #endregion
+
+    #region Debug
+
+    private void OnGUI()
+    {
+        var isVisible = IsVisible();
+        GUI.Box(new Rect(20, 20, 180, 23), $"isVisible = {isVisible}");
+    }
+
+    #endregion
+}
---
+++ Assets/Miyoshi/Scripts/EnemyRotation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRotation : MonoBehaviour
+{
+    [SerializeField] private int max_Interval = 5; // 回転間隔(秒)
+    [SerializeField] private int min_Interval = 1; // 回転間隔(秒)
+    [SerializeField] private float backInterval = 0.5f; // 後ろを向く間隔(秒)
+    [SerializeField] private float rotateAngleY = 45f; // 1回あたりの回転角度(Y)
+    bool isBack = false; // 後ろを向いているかどうか
+
+    public int interval = 3; // 回転間隔
+    private float timer;
+
+    void Update()
+    {
+        // 経過時間を加算
+        timer += Time.deltaTime;
+         // 回転間隔をランダムに設定
+        // 一定時間経過したら回転
+        if (timer >= interval)
+        {
+            
+            // 現在のRotationを取得してY軸に加算
+            transform.rotation = Quaternion.Euler(
+                transform.rotation.eulerAngles.x,
+                transform.rotation.eulerAngles.y + rotateAngleY,
+                transform.rotation.eulerAngles.z
+            );
+            Debug.Log(interval); // デバッグログ出力
+            isBack = true; // 後ろを向くかどうかを切り替え
+            // タイマーをリセット
+            timer = 0f;
+            interval = 0; // 回転間隔をリセット
+            interval = Random.Range(min_Interval, max_Interval);
+        }
+        if (isBack)
+        { // 後ろを向いている場合は回転しない
+            if (timer >= backInterval)
+            {
+                // 後ろを向く
+                transform.rotation = Quaternion.Euler(
+                    transform.rotation.eulerAngles.x,
+                    transform.rotation.eulerAngles.y + 180f,
+                    transform.rotation.eulerAngles.z
+                );
+                // タイマーをリセット
+                timer = 0f;
+                isBack = false; // 後ろを向く状態を解除
+            }
+        }
+
+    }
+}
