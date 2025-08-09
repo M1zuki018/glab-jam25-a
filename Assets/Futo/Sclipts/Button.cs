f3d0952f81bbed8f50cec63c35d8b4b7214c9851
futo aoi <faoi0803@icloud.com>
2025-08-09 13:52:28 +0900

    [Add]シーン移行を追加

---
+++ Assets/Futo/Sclipts/Button.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] string _changeSceneName = string.Empty;
+    [SerializeField] GameManager _gameManager;
+    [SerializeField] Color _color;
+    private void Start()
+    {
+        _gameManager = GameManager.FindAnyObjectByType<GameManager>();
+    }
+    public void OnClick()
+    {
+        _gameManager.ChangeScene(_changeSceneName,_color);
+    }
+}
